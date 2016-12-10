450eaee6d8875970e1d26946891d46eff1ebea34
Jez Humble <jez.humble@gsa.gov>
2016-12-09 21:37:00 -0800

    And that's a wrap. First draft of the workshop is done.

--- code/app/status.php
+++ code/app/status.php
@@ -2,7 +2,7 @@
 $db_config = getenv("HTTP_DB_URI");
 $db_hostname = explode(":", getenv("HTTP_DB_URI"));
 try {
-    $connection = new PDO("mysql:host=".$db_hostname[0].";dbname=workshop;charset=utf8", "workshop", getenv("HTTP_DB_PASSWORD"));
+    $connection = new PDO("mysql:host=".$db_hostname[0].";dbname=db18fworkshop;charset=utf8", "workshop", getenv("HTTP_DB_PASSWORD"));
 } catch (Exception $e) {
     http_response_code(500);
     error_log("Couldn't create database connection with URI: ".$db_config." Error: ".$e->getMessage());
